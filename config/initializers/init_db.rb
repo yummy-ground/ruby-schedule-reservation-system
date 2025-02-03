0c9b391071c29a10bef1edbdb0413de33a551188
yummygyudon <bang2brew@gmail.com>
2025-02-03 18:44:21 +0900

    fix : Fix test CI workflow
    `db:*` 패턴 명령을 직접 실행하지 않고 test 실행 시, 의도한 initialize가 될 수 있도록 설정했습니다.
    - 정해져있는 `db:*` 명령에 의해 해당 명령을 위해 필요로 하지만 실제로 필요하지 않은 리소스 파일들에 대한 생성을 방지하기 위함입니다.

--- config/initializers/init_db.rb
+++ config/initializers/init_db.rb
@@ -2,8 +2,21 @@ if Rails.env.development?
   Rails.application.config.after_initialize do
     require "rake"
     Rails.application.load_tasks
-    Rake::Task["db:create"].invoke
-    Rake::Task["db:migrate"].invoke
-    Rake::Task["db:seed:replant"].invoke
+    unless ActiveRecord::Base.connection.data_source_exists?("schema_migrations")
+      Rake::Task["db:create"].invoke
+      Rake::Task["db:migrate"].invoke
+      Rake::Task["db:seed:replant"].invoke
+    end
+  end
+end
+
+if Rails.env.test?
+  Rails.application.config.after_initialize do
+    require "rake"
+    Rails.application.load_tasks
+    unless ActiveRecord::Base.connection.data_source_exists?("schema_migrations")
+      Rake::Task["db:create"].invoke
+      Rake::Task["db:migrate"].invoke
+    end
   end
 end
