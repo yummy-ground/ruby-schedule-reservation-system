bc2f5179c61fbdfb76a71cd69c47b88be651ee05
yummygyudon <bang2brew@gmail.com>
2025-02-05 20:37:29 +0900

    add : Add Error/Success Module data for additional case
    API 호출하는 과정에서 발생할 수 있는 성공/실패 사례에 대한 데이터를 추가했습니다.

--- app/controllers/code/failure.rb
+++ app/controllers/code/failure.rb
@@ -1,16 +1,26 @@
-
 FailureData = Struct.new(:status_code, :message, keyword_init: true)
+
 module Failure
   # 400
   NO_TOKEN_IN_HEADER = FailureData.new(status_code: 400, message: "인증 토큰이 없습니다.")
+  INVALID_PARAMETER = FailureData.new(status_code: 400, message: "잘못된 파라미터 값입니다.")
+  IMPOSSIBLE_CREATE_TOO_LATE = FailureData.new(status_code: 400, message: "시작 전 3일 이내에는 예약이 불가능합니다.")
+  IMPOSSIBLE_UPDATE_ALREADY_CONFIRM = FailureData.new(status_code: 400, message: "이미 확정된 예약은 수정이 불가능합니다.")
+  IMPOSSIBLE_DELETE_ALREADY_CONFIRM = FailureData.new(status_code: 400, message: "이미 확정된 예약은 삭제가 불가능합니다.")
+  OVER_PERSONNEL_TO_RESERVE = FailureData.new(status_code: 400, message: "최대 인원 초과로 예약이 불가능합니다.")
+  OVER_PERSONNEL_TO_CONFIRM = FailureData.new(status_code: 400, message: "최대 인원 초과로 확정이 불가능합니다.")
 
   # 401
   INVALID_TOKEN_IN_HEADER =  FailureData.new(status_code: 400, message: "인증 토큰이 유효하지 않습니다.")
 
   # 403
+  NO_PERMISSION_SHOW = FailureData.new(status_code: 403, message: "예약 상세 조회 권한이 없습니다.")
   NO_PERMISSION_CONFIRM =  FailureData.new(status_code: 403, message: "예약 확정 권한이 없습니다.")
+  NO_PERMISSION_UPDATE = FailureData.new(status_code: 403, message: "예약 수정 권한이 없습니다.")
+  NO_PERMISSION_DELETE = FailureData.new(status_code: 403, message: "예약 삭제 권한이 없습니다.")
 
   # 404
   NOT_FOUND_USER =  FailureData.new(status_code: 404, message: "해당 유저를 찾을 수 없습니다.")
+  NOT_FOUND_SCHEDULE =  FailureData.new(status_code: 404, message: "해당 예약 일정을 찾을 수 없습니다.")
 end
 
--- app/controllers/code/success.rb
+++ app/controllers/code/success.rb
@@ -2,6 +2,7 @@ SuccessData = Struct.new(:status_code, :message, keyword_init: true)
 
 module Success
   # 200
+  GET_RESERVATION_DETAIL = SuccessData.new(status_code: 200, message: "예약 상세 조회를 성공했습니다.")
   GET_RESERVATION_ALL = SuccessData.new(status_code: 200, message: "예약 내역 조회를 성공했습니다.")
   GET_RESERVATION_AVAILABILITY_TIME = SuccessData.new(status_code: 200, message: "예약 가능 시간 조회를 성공했습니다.")
 
