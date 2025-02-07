e9df801cdec22f2df6e5b37a86763f8fc0e38c6d
yummygyudon <bang2brew@gmail.com>
2025-02-07 19:21:49 +0900

    fix : Add `db:create` command in `init_app`

--- config/initializers/init_app.rb
+++ config/initializers/init_app.rb
@@ -3,7 +3,10 @@ require "rake"
 if Rails.env.development?
   Rails.application.config.after_initialize do
     Rails.application.load_tasks
-    Rake::Task["db:setup"].invoke
+    Rake::Task["db:create"].invoke
+    unless ActiveRecord::Base.connection.data_source_exists?("schema_migrations")
+      Rake::Task["db:setup"].invoke
+    end
     Rake::Task["jwt:generate_for_user"].invoke
     Rake::Task["jwt:generate_for_admin"].invoke
   end
